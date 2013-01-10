6657922a877d6178dafac0647c54bc64eae7b513
Rodolfo Carvalho <rhcarvalho@gmail.com>
2013-01-11 03:03:43 +0800

    DAWGWordSegmenter passes our tests.

--- test_shenmeci.py
+++ test_shenmeci.py
@@ -1,11 +1,11 @@
 # -*- coding: utf-8 -*-
 import unittest
-from shenmeci import WordSegmenter
+from shenmeci import DAWGWordSegmenter as WordSegmenter
 
 
 class BaseWordSegmenterTestCase(unittest.TestCase):
     def setUp(self):
-        vocabulary = set("""
+        vocabulary = set(u"""
         A
         AB
         ABCD
@@ -16,38 +16,38 @@ class BaseWordSegmenterTestCase(unittest.TestCase):
 
 class SegmentSingleWordTests(BaseWordSegmenterTestCase):
     def test_empty_input(self):
-        sentence = ""
+        sentence = u""
         self.assertEqual([], self.segmenter.segment(sentence))
     
     def test_single_character(self):
-        sentence = "A"
-        self.assertEqual(["A"], self.segmenter.segment(sentence))
+        sentence = u"A"
+        self.assertEqual([u"A"], self.segmenter.segment(sentence))
     
     def test_two_character_word(self):
-        sentence = "AB"
-        self.assertEqual(["AB"], self.segmenter.segment(sentence))
+        sentence = u"AB"
+        self.assertEqual([u"AB"], self.segmenter.segment(sentence))
 
 
 class SegmentTwoWordsTests(BaseWordSegmenterTestCase):
     def test_two_single_character_words(self):
-        sentence = "AC"
-        self.assertEqual(["A", "C"], self.segmenter.segment(sentence))
+        sentence = u"AC"
+        self.assertEqual([u"A", u"C"], self.segmenter.segment(sentence))
     
     def test_two_character_word_plus_single_character_word(self):
-        sentence = "ABC"
-        self.assertEqual(["AB", "C"], self.segmenter.segment(sentence))
+        sentence = u"ABC"
+        self.assertEqual([u"AB", u"C"], self.segmenter.segment(sentence))
 
 
 class SegmentThreeWordsTests(BaseWordSegmenterTestCase):
     def test_three_single_character_words(self):
-        sentence = "ACD"
-        self.assertEqual(["A", "C", "D"], self.segmenter.segment(sentence))
+        sentence = u"ACD"
+        self.assertEqual([u"A", u"C", u"D"], self.segmenter.segment(sentence))
 
 
 class SegmentLongWordsTests(BaseWordSegmenterTestCase):
     def test_long_word_prefix_wont_match(self):
-        sentence = "ABCDZ"
-        self.assertEqual(["ABCD", "Z"], self.segmenter.segment(sentence))
+        sentence = u"ABCDZ"
+        self.assertEqual([u"ABCD", u"Z"], self.segmenter.segment(sentence))
 
 
 if __name__ == '__main__':
