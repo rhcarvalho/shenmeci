fddb0241abacca2f4be49964dec3dac54616883f
Rodolfo Carvalho <rhcarvalho@gmail.com>
2013-01-10 16:20:43 +0800

    Add license information in source files.

--- webapp.py
+++ webapp.py
@@ -1,5 +1,25 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
+
+# Shenmeci: Chinese word segmentation and Chinese-English online dictionary.
+# Copyright (C) 2013  Rodolfo Henrique Carvalho
+# https://github.com/rhcarvalho/shenmeci
+#
+# This file is part of Shenmeci.
+#
+# Shenmeci is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
 import os
 from bottle import route, request, static_file
 
