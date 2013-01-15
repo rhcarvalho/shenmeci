b99117c7d35516f0a4243f53454dd49209dd7c47
Rodolfo Carvalho <rhcarvalho@gmail.com>
2013-01-15 15:54:21 +0800

    Add script to download SIGHAN's 2005 Bakeoff data.

---
+++ download_bakeoff_data.sh
@@ -0,0 +1,23 @@
+#!/usr/bin/env bash
+
+# Shenmeci: Chinese word segmentation and Chinese-English online dictionary.
+# Copyright (C) 2013  Rodolfo Henrique Carvalho
+# https://github.com/rhcarvalho/shenmeci
+#
+# This file is part of Shenmeci.
+#
+# Shenmeci is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+URL=http://s.rodolfocarvalho.net/ch/icwb2-data.tar.bz2
+wget -c $URL && tar xjvf $(basename $URL)
