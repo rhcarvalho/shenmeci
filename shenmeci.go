a794c64da53b93fdd5b7f7dbc6613a529666f9f9
Rodolfo Carvalho <rhcarvalho@gmail.com>
2013-03-29 19:26:07 +0800

    Show pinyin using unicode characters instead of numbers.

--- shenmeci.go
+++ shenmeci.go
@@ -12,6 +12,7 @@ import (
 	"net/http"
 	"os"
 	"os/exec"
+	"strconv"
 	"strings"
 )
 
@@ -117,7 +118,7 @@ func loadCEDICT(filename string) (c *CEDICT, err error) {
 		pinyin := string(parts[2][bytes.IndexByte(parts[2], '[')+1 : bytes.IndexByte(parts[2], ']')])
 		entry := c.Dict[wordSimplified]
 		entry.definitions = append(entry.definitions, meaning)
-		entry.pinyin = append(entry.pinyin, pinyin)
+		entry.pinyin = append(entry.pinyin, pinyinNumberedToUnicode(pinyin))
 		c.Dict[wordSimplified] = entry
 		c.Dawg.Insert(wordSimplified)
 		if wordLen := len([]rune(wordSimplified)); wordLen > c.MaxKeyLen {
@@ -128,6 +129,60 @@ func loadCEDICT(filename string) (c *CEDICT, err error) {
 		}
 	}
 	return
+}
+
+// pinyinNumberedSyllableToUnicode converts a single
+// pinyin syllabe from numbered form to unicode.
+// Example: zhong1 -> zhōng
+func pinyinNumberedSyllableToUnicode(pinyin string) string {
+	var (
+		a = []rune("āáǎàa")
+		e = []rune("ēéěèe")
+		i = []rune("īíǐìi")
+		o = []rune("ōóǒòo")
+		u = []rune("ūúǔùu")
+		v = []rune("ǖǘǚǜü")
+	)
+	if len := len(pinyin); len > 0 {
+		last := pinyin[len-1:]
+		tone, err := strconv.Atoi(last)
+		if err == nil && 1 <= tone && tone <= 5 {
+			pinyin = pinyin[:len-1]
+			switch {
+			case strings.Contains(pinyin, "a"):
+				pinyin = strings.Replace(pinyin, "a", string(a[tone-1]), 1)
+			case strings.Contains(pinyin, "e"):
+				pinyin = strings.Replace(pinyin, "e", string(e[tone-1]), 1)
+			case strings.Contains(pinyin, "ou"):
+				pinyin = strings.Replace(pinyin, "o", string(o[tone-1]), 1)
+			case strings.Contains(pinyin, "io"):
+				pinyin = strings.Replace(pinyin, "o", string(o[tone-1]), 1)
+			case strings.Contains(pinyin, "iu"):
+				pinyin = strings.Replace(pinyin, "u", string(u[tone-1]), 1)
+			case strings.Contains(pinyin, "ui"):
+				pinyin = strings.Replace(pinyin, "i", string(i[tone-1]), 1)
+			case strings.Contains(pinyin, "uo"):
+				pinyin = strings.Replace(pinyin, "o", string(o[tone-1]), 1)
+			case strings.Contains(pinyin, "i"):
+				pinyin = strings.Replace(pinyin, "i", string(i[tone-1]), 1)
+			case strings.Contains(pinyin, "o"):
+				pinyin = strings.Replace(pinyin, "o", string(o[tone-1]), 1)
+			case strings.Contains(pinyin, "u:"):
+				pinyin = strings.Replace(pinyin, "u:", string(v[tone-1]), 1)
+			case strings.Contains(pinyin, "u"):
+				pinyin = strings.Replace(pinyin, "u", string(u[tone-1]), 1)
+			}
+		}
+	}
+	return pinyin
+}
+
+func pinyinNumberedToUnicode(pinyin string) string {
+	syllables := strings.Split(pinyin, " ")
+	for i, syllable := range syllables {
+		syllables[i] = pinyinNumberedSyllableToUnicode(syllable)
+	}
+	return strings.Join(syllables, " ")
 }
 
 var (
--- shenmeci_test.go
+++ shenmeci_test.go
@@ -84,3 +84,15 @@ func newDAWGFromCEDICT() (d *dawg.DAWG, err error) {
 	}
 	return
 }
+
+func TestPinyin(t *testing.T) {
+	if r := pinyinNumberedSyllableToUnicode("zhong1"); r != "zhōng" {
+		t.Errorf("expected %v got %v", "zhōng", r)
+	}
+	if r := pinyinNumberedToUnicode("zhong1 guo2"); r != "zhōng guó" {
+		t.Errorf("expected %v got %v", "zhōng guó", r)
+	}
+	if r := pinyinNumberedToUnicode("U S B shou3 zhi3"); r != "U S B shǒu zhǐ" {
+		t.Errorf("expected %v got %v", "U S B shǒu zhǐ", r)
+	}
+}
