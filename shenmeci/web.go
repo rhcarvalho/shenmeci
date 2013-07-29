20b15d9cd31eba144f6fc94eed2330b7ac49bb14
Rodolfo Carvalho <rhcarvalho@gmail.com>
2013-07-29 17:15:27 +0200

    Store information about every query on MongoDB.
    Closes #11.

--- shenmeci/web.go
+++ shenmeci/web.go
@@ -2,12 +2,24 @@ package main
 
 import (
 	"encoding/json"
+	"labix.org/v2/mgo"
+	"labix.org/v2/mgo/bson"
 	"log"
 	"net/http"
 	"strings"
+	"time"
 )
 
+type QueryRecord struct {
+	Query       string
+	Result      []map[string]string
+	When        time.Time
+	Duration    time.Duration
+	RequestInfo *http.Request
+}
+
 func segmentHandler(w http.ResponseWriter, r *http.Request) {
+	startTime := time.Now()
 	query := r.FormValue("q")
 	results := keysToResults(func() (keys []string) {
 		for _, key := range segment(cedict.Dawg, []rune(query)) {
@@ -25,6 +37,11 @@ func segmentHandler(w http.ResponseWriter, r *http.Request) {
 	}
 	b, _ := json.Marshal(map[string]interface{}{"r": results})
 	w.Write(b)
+	duration := time.Since(startTime)
+
+	// Insert into MongoDB in another goroutine.
+	// This finishes the response without blocking.
+	go collection.Insert(&QueryRecord{query, results, bson.Now(), duration, r})
 }
 
 func keysToResults(keys []string) (results []map[string]string) {
@@ -47,14 +64,27 @@ func keysToResults(keys []string) (results []map[string]string) {
 	return results
 }
 
+var collection *mgo.Collection
+
 func serve(host, port string) {
+	session, err := mgo.Dial("localhost")
+	if err != nil {
+		panic(err)
+	}
+	defer session.Close()
+
+	// Optional. Switch the session to a monotonic behavior.
+	session.SetMode(mgo.Monotonic, true)
+
+	collection = session.DB("shenmeci").C("queries")
+
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
 		http.ServeFile(w, r, "./static/index.html")
 	})
 	http.Handle("/static/", http.FileServer(http.Dir(".")))
 	http.HandleFunc("/segment", segmentHandler)
 	log.Printf("Serving on PORT=%v", port)
-	err := http.ListenAndServe(host+":"+port, nil)
+	err = http.ListenAndServe(host+":"+port, nil)
 	if err != nil {
 		log.Fatal(err)
 	}
