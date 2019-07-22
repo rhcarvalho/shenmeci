ab72b87d622313770e766d3b0b4e52f198f746a7
Rodolfo Carvalho <rhcarvalho@gmail.com>
2019-07-22 19:46:58 +0200

    Load CEDICT from known place during tests
    This makes running tests easier, without requiring a config.json file.

--- internal/shenmeci/shenmeci_test.go
+++ internal/shenmeci/shenmeci_test.go
@@ -1,17 +1,22 @@
 package shenmeci
 
 import (
+	"os"
+	"path/filepath"
 	"testing"
 )
 
 func BenchmarkLoadCEDICT(b *testing.B) {
+	p := filepath.Join("..", "..", "dict", "cedict_1_0_ts_utf-8_mdbg.txt.gz")
+	fi, err := os.Stat(p)
+	if err != nil {
+		b.Skipf("CEDICT file %q not available: %v", p, err)
+	}
+	if fi.IsDir() {
+		b.Skipf("CEDICT file %q not available: wanted file, got directory", p)
+	}
+	GlobalConfig.CedictPath = p
 	for i := 0; i < b.N; i++ {
 		LoadCEDICT()
 	}
-}
-
-func init() {
-	LoadConfig()
-	ValidateConfig()
-	LoadCEDICT()
 }
