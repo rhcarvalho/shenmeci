b6155e99cbd9c0f3d43f4c2fe91a5f0e68248f42
Rodolfo Carvalho <rhcarvalho@gmail.com>
2019-07-25 21:57:48 +0200

    Reorganize segmentation tests using subtests

--- internal/segmentation/segmentation_test.go
+++ internal/segmentation/segmentation_test.go
@@ -7,48 +7,29 @@ import (
 
 var segmenter = NewSegmenter(strings.Fields("语言 信息 处理 世界"))
 
-func TestSegmentEmpty(t *testing.T) {
-	testSegment("", [][]rune{}, t)
-}
-
-func TestSegmentChinese(t *testing.T) {
-	sentence := "语言信息处理"
-	expectedWords := [][]rune{
-		[]rune("语言"),
-		[]rune("信息"),
-		[]rune("处理"),
+func TestSegment(t *testing.T) {
+	tests := []struct {
+		name string
+		text string
+		want []string
+	}{
+		{"Empty", "", nil},
+		{"Chinese", "语言信息处理", strings.Fields("语言 信息 处理")},
+		{"NonChinese", "I am a sentence in English.", []string{"I am a sentence in English."}},
+		{"EnglishChinese", "Hello 世界.", []string{"Hello ", "世界", "."}},
 	}
-	testSegment(sentence, expectedWords, t)
-}
-
-func TestNonChinese(t *testing.T) {
-	sentence := "I am a sentence in English."
-	expectedWords := [][]rune{
-		[]rune("I am a sentence in English."),
-	}
-	testSegment(sentence, expectedWords, t)
-}
-
-func TestEnglishChinese(t *testing.T) {
-	sentence := "Hello 世界."
-	expectedWords := [][]rune{
-		[]rune("Hello "),
-		[]rune("世界"),
-		[]rune("."),
-	}
-	testSegment(sentence, expectedWords, t)
-}
-
-func testSegment(sentence string, expectedWords [][]rune, t *testing.T) {
-	words := segmenter.Segment([]rune(sentence))
-	if len(words) != len(expectedWords) {
-		t.Errorf("segmented %q should be %q, got %q", sentence, expectedWords, words)
-	}
-	for i, word := range words {
-		if string(word) != string(expectedWords[i]) {
-			t.Errorf("segmented %q should be %q, got %q\nfirst differing word [%d]: %q != %q",
-				sentence, expectedWords, words, i, expectedWords[i], word)
-		}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			got := segmenter.Segment([]rune(tt.text))
+			if len(got) != len(tt.want) {
+				t.Fatalf("got %d segments, want %d", len(got), len(tt.want))
+			}
+			for i := range got {
+				if string(got[i]) != tt.want[i] {
+					t.Fatalf("[segment#%d] got %q, want %q", i+1, string(got[i]), tt.want[i])
+				}
+			}
+		})
 	}
 }
 
